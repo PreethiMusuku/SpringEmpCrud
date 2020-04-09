2fee7146c96a0491f53df7ad7369b1ed5d360efa
Preethi Musuku <preethimusuku@gmail.com>
2020-04-09 23:17:21 +0530

    with actuators and exceptions

---
+++ EmployeeCrudWithDataJpa/src/main/java/com/cap/service/EmpServiceImpl.java
@@ -0,0 +1,25 @@
+package com.cap.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.http.ResponseEntity;
+
+import com.cap.entity.Employee;
+
+
+
+public interface EmpServiceImpl {
+	String createEmployee(Employee emp);
+
+	Optional<Employee> findEmployee(Integer empId);
+
+	List<Employee> findAllEmployees();
+
+	List<Employee> findEmpByName(String empName);
+
+	String updateEmployee(Employee emp);
+
+	String deleteEmpById(Integer eId);
+
+}
